ecdc92afce001191d2c91ba846438241a976fe68
魏兴泰 <xingtai.wei@dreamsmart.com>
2025-03-11 17:00:33 +0800

    [MzHotFix] #0 移除多余文件

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,7 +5,7 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_meizu_mzhotfix_SOFileManager_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "这是一段通过So返回的未更新的字符串";
-//    std::string hello = "这是一段通过So返回的已更新的字符串";
+    std::string hello = "这是一段通过So返回的修复前的字符串";
+//    std::string hello = "这是一段通过So返回的修复后的字符串";
     return env->NewStringUTF(hello.c_str());
 }
